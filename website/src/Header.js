97c5b9549e7c028f6f2c575616101ef21a30c5b6
sleepycobbler <sleepycobbler@gmail.com>
2021-12-12 23:53:09 -0700

    first steps

---
+++ website/src/Header.js
@@ -0,0 +1,8 @@
+const Header = () => (
+    <div className="Sleepy-header">
+      <div className="header-title"><h1>The Sleepy Cobbler's Workshop</h1></div>
+      <div className="drop-downs"><h2>My Stuff</h2></div>
+    </div>
+  )
+  
+  export default Header;
---
+++ website/src/Stuff.js
@@ -0,0 +1,7 @@
+const Stuff = () => (
+    <div className="Sleepy-stuff">
+      <div><h2>My Stuff:</h2></div>
+    </div>
+  )
+  
+  export default Stuff;
